bd63ede770375ca97b90d494162555514af80f61
James Legg <james.legg@conversocial.com>
2020-09-29 18:11:52 +0100

    fix: use correct path for script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ cd $GITHUB_WORKSPACE
 
 helm repo add helm-private $HELM_PRIVATE_REPO_URL --username $NEXUS3_USERNAME --password $NEXUS3_PASSWORD
 
-python3 bin/helm-repo-add.py
+python3 /helm-repo-add.py
 
 helm repo list
 
